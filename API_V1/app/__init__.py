75ce9e2fdc8900f99d76d9c9f4238351e277ec24
Darius-Ndubi <ndubidarius@gmail.com>
2018-09-04 23:43:11 +0300

    [Feature #160230987] Adds get all orders Adds tsts to get all orders endpoint Adds travis configuration

---
+++ API_V1/app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_restplus import Api
+from werkzeug.contrib.fixers import ProxyFix
+
+app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
+api = Api(app, version='1.0', title='Fast Food Fast API',
+    description='Fast-Food-Fast is a food delivery service app for a restaurant',)
+
+
+from resources.user_orders import ns as orders
+api.add_namespace(orders)
