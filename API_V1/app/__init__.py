68ce73dc4816602f583b9ac8844c52afcbd436c4
Darius-Ndubi <ndubidarius@gmail.com>
2018-09-15 18:06:18 +0300

    [Fix #160231068] Removes redundant code

--- API_V1/app/__init__.py
+++ API_V1/app/__init__.py
@@ -1,9 +1,7 @@
 from flask import Flask
 from flask_restplus import Api
-from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
-app.wsgi_app = ProxyFix(app.wsgi_app)
 
 api = Api(app, version='1.0', title='Fast Food Fast API',
     description='Fast-Food-Fast is a food delivery service app for a restaurant',)
