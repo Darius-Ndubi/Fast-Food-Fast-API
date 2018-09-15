7118cdf09fb7a8415c81d25d482f122e00f487c3
Darius-Ndubi <ndubidarius@gmail.com>
2018-09-15 19:10:55 +0300

    [Fix #160230654] Restructures Api for versioning

--- API_V1/app/__init__.py
+++ API_V1/app/__init__.py
@@ -1,16 +1,14 @@
 from flask import Flask
 from flask_restplus import Api
-from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
-app.wsgi_app = ProxyFix(app.wsgi_app)
 
 api = Api(app, version='1.0', title='Fast Food Fast API',
     description='Fast-Food-Fast is a food delivery service app for a restaurant',)
 
 
-from resources.user_auth import ns as auth
+from resources.v1.user_auth import ns as auth
 api.add_namespace(auth)
 
-from resources.user_orders import ns as orders
+from resources.v1.user_orders import ns as orders
 api.add_namespace(orders)
