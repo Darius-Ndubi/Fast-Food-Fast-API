aad0bd206f211450068d7b56a9a5cbae70a04e02
Darius-Ndubi <ndubidarius@gmail.com>
2018-09-22 00:06:23 +0300

    [Fix #160295024] restructures the app structure Moves tests folder to the parent structure

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,7 @@ from flask_restplus import Api
 #flask instance
 app = Flask(__name__)
 
-api = Api(app, version='1.0', title='Fast Food Fast API',
-    description='Fast-Food-Fast is a food delivery service app for a restaurant',)
+api = Api(app, version='1.0', title='Fast Food Fast API', description='Fast-Food-Fast is a food delivery service app for a restaurant')
 
 
 from app.a_p_i.v1.views.order_views import ns as orders
