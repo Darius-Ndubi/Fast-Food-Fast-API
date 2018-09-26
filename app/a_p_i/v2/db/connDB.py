2bcd9ed302c19cca3300b0a18d303f29148dce06
Darius-Ndubi <ndubidarius@gmail.com>
2018-09-26 09:47:28 +0300

    [Chore #160782169] Set up the db Set up test db Set up procuction db set up db for travis build

---
+++ app/a_p_i/v2/db/connDB.py
@@ -0,0 +1,15 @@
+"""module to handle connections to the diffrent DBs"""
+
+import psycopg2
+import os
+
+
+def connDb():
+    """function to set up connection to either of the dbs
+    """
+    connectdb = os.getenv('TDB_URL')
+    #connectdb = os.getenv('DB_URL')
+    try:
+        return psycopg2.connect(connectdb)
+    except:
+        print("Sorry connection to database failed")
