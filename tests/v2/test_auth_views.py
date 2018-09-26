642aa6fcc5619949d4cca2da8ec39575c1e03980
Darius-Ndubi <ndubidarius@gmail.com>
2018-09-27 00:26:22 +0300

    [Feature #160782280] Failing tests on login endpoints add failing tests for signin endpoint

--- tests/v2/test_auth_views.py
+++ tests/v2/test_auth_views.py
@@ -1,6 +1,7 @@
 """Test module on user authentication endpoints"""
 import pytest
 from flask import json
+from flask_jwt_extended import create_access_token
 
 # local imports
 from app import app
@@ -27,9 +28,27 @@ def registered():
 
     # close the connection
     curs.close()
-    connection.commit()
     connection.close()
     return len(all)
+
+
+def id_picker():
+    """Function to pick the users id from db that will be used to create the access token"""
+    connection = connDb()
+    curs = connection.cursor()
+    curs.execute(
+        "SELECT * FROM users WHERE username = {}".format(mock_log[6]['email']))
+    user_data = curs.fetchall()
+    curs.close()
+    connection.close()
+    return user_data['user_id']
+
+
+def create_token():
+    """Method to creat test token for the user"""
+    u_id = id_picker()
+    user_access_token = create_access_token(u_id)
+    return user_access_token
 
 
 """
@@ -42,7 +61,7 @@ def test_signup_empty_email(client):
     response = client.post('/api/v2/auth/signup',
                            data=json.dumps(mock_reg[0]), content_type='application/json')
     json.loads(response.data.decode('utf-8'))
-    assert response.json == {'message' : error_messages[2]['Invalid_email']}
+    assert response.json == {'message': error_messages[2]['Invalid_email']}
     assert(response.status_code == 400)
 
 
@@ -51,7 +70,7 @@ def test_signup_wrong_email1(client):
     response = client.post('/api/v2/auth/signup',
                            data=json.dumps(mock_reg[1]), content_type='application/json')
     json.loads(response.data.decode('utf-8'))
-    assert response.json == {'message' : error_messages[2]['Invalid_email']}
+    assert response.json == {'message': error_messages[2]['Invalid_email']}
     assert(response.status_code == 400)
 
 
@@ -60,7 +79,7 @@ def test_signup_wrong_email2(client):
     response = client.post('/api/v2/auth/signup',
                            data=json.dumps(mock_reg[2]), content_type='application/json')
     json.loads(response.data.decode('utf-8'))
-    assert response.json == {'message' : error_messages[2]['Invalid_email']}
+    assert response.json == {'message': error_messages[2]['Invalid_email']}
     assert(response.status_code == 400)
 
 
@@ -69,7 +88,7 @@ def test_signup_int_email(client):
     response = client.post('/api/v2/auth/signup',
                            data=json.dumps(mock_reg[3]), content_type='application/json')
     json.loads(response.data.decode('utf-8'))
-    assert response.json == {'message' : error_messages[3]['Incorrect_email']}
+    assert response.json == {'message': error_messages[3]['Incorrect_email']}
     assert(response.status_code == 400)
 
 
@@ -83,7 +102,7 @@ def test_signup_int_password(client):
     response = client.post('/api/v2/auth/signup',
                            data=json.dumps(mock_reg[5]), content_type='application/json')
     json.loads(response.data.decode('utf-8'))
-    assert response.json == {'message' : error_messages[4]['incorrect_passwd']}
+    assert response.json == {'message': error_messages[4]['incorrect_passwd']}
     assert(response.status_code == 400)
 
 
@@ -92,7 +111,7 @@ def test_signup_passwords_unmatching(client):
     response = client.post('/api/v2/auth/signup',
                            data=json.dumps(mock_reg[6]), content_type='application/json')
     json.loads(response.data.decode('utf-8'))
-    assert response.json == {'message' : error_messages[5]['unmatching']}
+    assert response.json == {'message': error_messages[5]['unmatching']}
     assert(response.status_code == 400)
 
 
@@ -101,7 +120,7 @@ def test_signup_poor_passwords(client):
     response = client.post('/api/v2/auth/signup',
                            data=json.dumps(mock_reg[7]), content_type='application/json')
     json.loads(response.data.decode('utf-8'))
-    assert response.json == {'message' : error_messages[6]['poor_pass']}
+    assert response.json == {'message': error_messages[6]['poor_pass']}
     assert(response.status_code == 400)
 
 
@@ -115,7 +134,7 @@ def test_signup_int_username(client):
     response = client.post('/api/v2/auth/signup',
                            data=json.dumps(mock_reg[8]), content_type='application/json')
     json.loads(response.data.decode('utf-8'))
-    assert response.json == {'message' : error_messages[7]['invalid_uname']}
+    assert response.json == {'message': error_messages[7]['invalid_uname']}
     assert(response.status_code == 400)
 
 
@@ -124,7 +143,7 @@ def test_signup_empty_username(client):
     response = client.post('/api/v2/auth/signup',
                            data=json.dumps(mock_reg[9]), content_type='application/json')
     json.loads(response.data.decode('utf-8'))
-    assert response.json == {'message' : error_messages[8]['poor_uname']}
+    assert response.json == {'message': error_messages[8]['poor_uname']}
     assert(response.status_code == 400)
 
 
@@ -133,9 +152,8 @@ def test_signup_spaces_username(client):
     response = client.post('/api/v2/auth/signup',
                            data=json.dumps(mock_reg[10]), content_type='application/json')
     json.loads(response.data.decode('utf-8'))
-    assert response.json == {'message' : error_messages[8]['poor_uname']}
+    assert response.json == {'message': error_messages[8]['poor_uname']}
     assert(response.status_code == 400)
-    
 
 
 def test_signup_correct_data(client):
@@ -148,6 +166,75 @@ def test_signup_correct_data(client):
     assert old_num_users + 1 == new_num_users
     assert response.get_json() == success_messages[0]['account_created']
     assert (response.status_code == 201)
+
+
+"""
+    User Sign In Tests
+    -> Email tests
+    -> password tests
+"""
+
+
+def test_login_int_email(client):
+    """Test input of int as email"""
+    response = client.post('/api/v2/auth/login',
+                           data=json.dumps(mock_log[0]), content_type='application/json')
+    assert response.json == {'message': error_messages[3]['Incorrect_email']}
+    assert(response.status_code == 400)
+
+
+def test_login_empty_email(client):
+    """Tests input of empty string as email"""
+    response = client.post('/api/v2/auth/login',
+                           data=json.dumps(mock_log[1]), content_type='application/json')
+    assert response.json == {'message': error_messages[2]['Invalid_email']}
+    assert(response.status_code == 400)
+
+
+def test_login_wrong_email1(client):
+    """Tests input of email without @ """
+    response = client.post('/api/v2/auth/login',
+                           data=json.dumps(mock_log[2]), content_type='application/json')
+    assert response.json == {'message': error_messages[2]['Invalid_email']}
+    assert(response.status_code == 400)
+
+
+def test_login_wrong_email2(client):
+    """Tests input of email without .com"""
+    response = client.post('/api/v2/auth/login',
+                           data=json.dumps(mock_log[3]), content_type='application/json')
+    assert response.json == {'message': error_messages[2]['Invalid_email']}
+    assert(response.status_code == 400)
+
+
+"""
+    Password Checks
+"""
+
+
+def test_login_int_password(client):
+    """Test int input as password"""
+    response = client.post('/api/v2/auth/login',
+                           data=json.dumps(mock_log[4]), content_type='application/json')
+    assert response.json == {'message': error_messages[4]['incorrect_passwd']}
+    assert(response.status_code == 400)
+
+
+def test_login_poor_password(client):
+    """Test input of short password"""
+    response = client.post('/api/v2/auth/login',
+                           data=json.dumps(mock_log[5]), content_type='application/json')
+    assert response.json == {'message': error_messages[6]['poor_pass']}
+    assert(response.status_code == 400)
+
+
+def test_login_known_user(client):
+    """Test of admin sign in
+    """
+    response = client.post(
+        '/api/v2/auth/login', data=json.dumps(mock_log[6]), content_type='application/json')
+    assert(response.status_code == 200)
+    assert response == create_token()
 
 
 """drop tables after testing"""
