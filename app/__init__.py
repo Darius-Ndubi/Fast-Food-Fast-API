d33e971914cf292e9565b69fe7d6b9b30788c032
Darius-Ndubi <ndubidarius@gmail.com>
2018-09-26 09:47:28 +0300

    [Chore #160782169] Set up the db Set up test db Set up procuction db set up db for travis build

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,23 @@
 from flask import Flask
 from flask_restplus import Api
 
+# local imports
+from app.a_p_i.v2.db.create_tables import create_dtb
+
+"""create the necesarry tables"""
+create_dtb()
+
 app = Flask(__name__)
 
 api = Api(app, version='1.0', title='Fast Food Fast API',
           description='Fast-Food-Fast is a food delivery service app for a restaurant',)
 
 
-from app.a_p_i.v1.views.user_views import ns as auth
-api.add_namespace(auth, path='/api/v1')
+# from app.a_p_i.v1.views.user_views import ns as auth
+# api.add_namespace(auth, path='/api/v1')
 
-from app.a_p_i.v1.views.food_views import ns as foods
-api.add_namespace(foods, path='/api/v1')
+# from app.a_p_i.v1.views.food_views import ns as foods
+# api.add_namespace(foods, path='/api/v1')
 
-from app.a_p_i.v1.views.order_views import ns as orders
-api.add_namespace(orders, path='/api/v1')
+# from app.a_p_i.v1.views.order_views import ns as orders
+# api.add_namespace(orders, path='/api/v1')
---
+++ app/a_p_i/v2/db/create_tables.py
@@ -0,0 +1,49 @@
+"""Module to create the required tables in database"""
+
+# local imports
+from app.a_p_i.v2.db.connDB import connDb
+
+
+def create_dtb():
+    tables = [
+        """ 
+        CREATE TABLE IF NOT EXISTS users(
+        user_id SERIAL PRIMARY KEY NOT NULL,
+        email VARCHAR NOT NULL,
+        phone_number INT NOT NULL,
+        username VARCHAR(10) NOT NULL,
+        password VARCHAR NOT NULL
+        )
+        """,
+        """
+        CREATE TABLE IF NOT EXISTS foods(
+        food_id SERIAL PRIMARY KEY NOT NULL,
+        title VARCHAR(20) NOT NULL,
+        description VARCHAR NOT NULL,
+        price INT NOT NULL,
+        type VARCHAR NOT NULL,
+        creator VARCHAR(20) NOT NULL
+        )
+        """,
+        """
+        CREATE TABLE IF NOT EXISTS orders(
+        order_id SERIAL PRIMARY KEY NOT NULL,
+        food_id INT NOT NULL REFERENCES foods(food_id),
+        title VARCHAR(20) NOT NULL,
+        price INT NOT NULL,
+        quantity INT NOT NULL,
+        total INT NOT NULL,
+        status VARCHAR NOT NULL,
+        creator VARCHAR(20) NOT NULL
+        )
+        """
+    ]
+    connection = connDb()
+    curs = connection.cursor()
+
+    for table in tables:
+        curs.execute(table)
+
+    curs.close()
+    connection.commit()
+    connection.close()
