0f70ff875961adab931e5bb6d5a415177fa74a51
Darius-Ndubi <ndubidarius@gmail.com>
2018-09-27 20:43:21 +0300

    [Feature #160782546] Add fuctionality to only allow admin to view all orders

--- app/a_p_i/v2/views/order_views.py
+++ app/a_p_i/v2/views/order_views.py
@@ -8,10 +8,12 @@ from app import api
 from app.a_p_i.v2.models.ManOrders import ManageOrdersDAO
 from app.a_p_i.v2.models.UserModel import ManageUserDAO
 from app.a_p_i.v1.views.order_views import order
+from app.a_p_i.v2.models.FoodModel import ManageFoodDAO
 
 ns = Namespace('orders', description='Orders and their operations')
 
 orderAO = ManageOrdersDAO()
+foodAO = ManageFoodDAO()
 
 
 """
@@ -50,3 +52,21 @@ class Order(Resource):
         new_order['username'] = ManageUserDAO.get_username(user_id)
 
         return orderAO.create_new_order(new_order), 201
+
+
+@ns.route('/orders/')
+@ns.response(200, 'This are the orders')
+@ns.response(400, 'Bad Request')
+@ns.response(404, 'No Orders yet')
+@ns.response(401, 'Please sign in First')
+class OrderList(Resource):
+    """
+        Class to get all orders orders created by a user
+    """
+    @ns.doc('List all orders made all users')
+    @jwt_required
+    def get(self):
+        user_id = get_jwt_identity()
+        '''Geting all posted orders'''
+        foodAO.admin_only(user_id)
+        return orderAO.find_all_orders(), 200
